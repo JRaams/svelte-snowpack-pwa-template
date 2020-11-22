932a2eb1e8f7122cafcd8a68e1bbe6fe0dc19c57
JRaams <47593569+JRaams@users.noreply.github.com>
2020-11-22 19:23:40 +0100

    Replace rollup with snowpack

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import App from './App.svelte';
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+	target: document.body
 });
 
 export default app;
+
+if (import.meta.hot) {
+	import.meta.hot.accept();
+	import.meta.hot.dispose(() => {
+		app.$destroy();
+	});
+}
